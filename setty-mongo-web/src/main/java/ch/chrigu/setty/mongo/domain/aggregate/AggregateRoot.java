53879abcc27b535b13e42037e85407e81335a8c1
huber.chrigu <huber.chrigu@gmail.com>
2019-02-01 14:20:28 +0100

    Every aggregate root should be comparable

--- setty-mongo-web/src/main/java/ch/chrigu/setty/mongo/domain/aggregate/AggregateRoot.java
+++ setty-mongo-web/src/main/java/ch/chrigu/setty/mongo/domain/aggregate/AggregateRoot.java
@@ -1,13 +1,18 @@
 package ch.chrigu.setty.mongo.domain.aggregate;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.annotation.Version;
 import org.springframework.hateoas.Identifiable;
 
 import java.util.Date;
 
 @Getter
-public class AggregateRoot implements Identifiable<String> {
+@EqualsAndHashCode
+public abstract class AggregateRoot implements Identifiable<String> {
     @Id
     private String id;
 
