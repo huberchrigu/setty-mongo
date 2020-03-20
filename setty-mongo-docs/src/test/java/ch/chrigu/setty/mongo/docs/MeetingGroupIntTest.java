2b7489f92fde9c29315d3e37df1db1e2d49f6f77
huber.chrigu <huber.chrigu@gmail.com>
2020-03-20 07:15:09 +0100

    Only the admin user can access a user without createdBy property

--- setty-mongo-docs/src/test/java/ch/chrigu/setty/mongo/docs/MeetingGroupIntTest.java
+++ setty-mongo-docs/src/test/java/ch/chrigu/setty/mongo/docs/MeetingGroupIntTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -52,6 +54,7 @@ public class MeetingGroupIntTest {
     }
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     public void meetingGroupShouldBeCreated() throws Exception {
         String body = Files.newBufferedReader(meetingGroupBody).lines().reduce("", (a, b) -> a + "\n" + b);
 
