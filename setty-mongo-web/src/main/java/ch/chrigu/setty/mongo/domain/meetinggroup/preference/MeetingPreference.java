3392a7041533a514bf477c2b9d12ac136a765eae
huber.chrigu <huber.chrigu@gmail.com>
2020-03-20 06:25:52 +0100

    Added debugging messages

--- setty-mongo-web/src/main/java/ch/chrigu/setty/mongo/domain/meetinggroup/preference/MeetingPreference.java
+++ setty-mongo-web/src/main/java/ch/chrigu/setty/mongo/domain/meetinggroup/preference/MeetingPreference.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.annotation.Transient;
 
 import java.time.DayOfWeek;
@@ -16,6 +18,8 @@ import java.util.Locale;
 @Getter
 @AllArgsConstructor
 public class MeetingPreference {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MeetingPreference.class);
+
     @JsonProperty(required = true)
     @NonNull
     private DayOfWeek day;
@@ -30,8 +34,9 @@ public class MeetingPreference {
 
     public CalendarEntry toCalendarEntry(int year, int week) {
         LocalDate from = LocalDate.now().withYear(year).with(weekFields.weekOfYear(), week).with(weekFields.dayOfWeek(), day.getValue());
+        LOGGER.info("from: {}", from);
         LocalDate to = from.plusDays(timeSpan.getDays());
-
+        LOGGER.info("timeSpan.getFrom(): {}", timeSpan.getFrom());
         return new CalendarEntry(from.atTime(timeSpan.getFrom()), to.atTime(timeSpan.getTo()));
     }
 }
